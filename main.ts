9f14f7e0acc45de6855b48fdc2b237028eff0564
playerdead01 <137476994+playerdead01@users.noreply.github.com>
2023-07-12 12:40:26 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.House)
+    } else {
+        basic.clearScreen()
+    }
+}
+basic.forever(function () {
+	
+})
